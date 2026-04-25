64f058ec12f26a4556a79305abe6a8ac21884ef8
MaxStrootmann <max@manndigital.nl>
2026-04-25 09:34:41 +0200

    Fix Neovim treesitter setup for 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -967,51 +967,45 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
+    config = function()
+      local parsers = {
         'bash',
         'c',
         'cmake',
         'cpp',
         'diff',
         'html',
+        'javascript',
+        'json',
         'lua',
         'luadoc',
         'markdown',
         'markdown_inline',
         'make',
+        'prisma',
+        'python',
         'query',
+        'tsx',
+        'typescript',
         'vim',
         'vimdoc',
-        'prisma',
-        'typescript',
-        'typescriptreact',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- bug with not auto starting treesitter on other filetypes
-    config = function()
+      }
+
+      require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath 'data' .. '/site',
+      }
+      require('nvim-treesitter').install(parsers)
+
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
-          -- do not start treesitter on dockerfiles
-          local dockerfile = vim.bo.filetype == 'dockerfile'
-          if dockerfile then
+          if vim.bo[args.buf].filetype == 'dockerfile' then
             return
           end
-          -- Only start treesitter if a parser exists
-          local ok = pcall(vim.treesitter.get_parser, args.buf)
-          if ok then
+
+          if pcall(vim.treesitter.get_parser, args.buf) then
             pcall(vim.treesitter.start, args.buf)
           end
         end,
